e02775c91c3b9c262e3c23b43a0e30c03b826b53
Zhenhua Cao <zhenhua.cao@certik.org>
2020-06-09 17:30:18 -0400

    test again

--- functions/test.js
+++ functions/test.js
@@ -2,16 +2,10 @@ require('dotenv').config();
 
 async function handler(event, context) {
   console.log(process.env);
-  console.log(event);
-  console.log(context);
 
   return {
     statusCode: 200,
-    body: JSON.stringify({
-      keys: Object.keys(process.env),
-      yy: process.env.BENBEN,
-      zz: process.env.HEY_TEST
-    })
+    body: JSON.stringify(process.env)
   };
 };
 
