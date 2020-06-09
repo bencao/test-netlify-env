5123a1fe9b706f242d3e3dab1ac8f6fdd0c4183b
Zhenhua Cao <zhenhua.cao@certik.org>
2020-06-09 16:31:15 -0400

    add print

--- functions/test.js
+++ functions/test.js
@@ -1,7 +1,13 @@
 const handler = function() {
+  console.log(process.env);
+
   return {
     statusCode: 200,
-    body: JSON.stringify(process.env)
+    body: JSON.stringify({
+      keys: Object.keys(process.env),
+      yy: process.env.BENBEN,
+      zz: process.env.HEY_TEST
+    })
   };
 };
 
