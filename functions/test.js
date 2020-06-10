e2100d605839588af016b157f18f9c7b6749398b
Zhenhua Cao <zhenhua.cao@certik.org>
2020-06-10 03:34:51 -0400

    just to trigger a new deploy preview

--- functions/test.js
+++ functions/test.js
@@ -4,7 +4,8 @@ async function handler(event, context) {
     body: JSON.stringify({
       BENBEN: process.env.BENBEN,
       CONTEXT: process.env.CONTEXT,
-      HAY_TEST: process.env.HAY_TEST
+      HAY_TEST: process.env.HAY_TEST,
+      BUILD_ID: process.env.BUILD_ID
     })
   };
 };
