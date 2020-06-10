42ebf07f926b01a95d8c03b38c90d3628e28b055
Zhenhua Cao <zhenhua.cao@certik.org>
2020-06-10 02:58:50 -0400

    add more output

--- functions/test.js
+++ functions/test.js
@@ -3,6 +3,7 @@ async function handler(event, context) {
     statusCode: 200,
     body: JSON.stringify({
       BENBEN: process.env.BENBEN,
+      CONTEXT: process.env.CONTEXT,
       HAY_TEST: process.env.HAY_TEST
     })
   };
