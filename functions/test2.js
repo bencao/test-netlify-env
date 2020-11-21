4ac757be24484a4bd9de39dbe2f38b6d6eaa2cd2
Zhenhua Cao <zhenhua.cao@certik.org>
2020-11-21 02:50:34 -0500

    add a second file to create duplication

---
+++ functions/test2.js
@@ -0,0 +1,14 @@
+require("./deep/test");
+
+async function handler(event, context) {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      BENBEN: process.env.BENBEN,
+      CONTEXT: process.env.CONTEXT,
+      HAY_TEST: process.env.HAY_TEST
+    })
+  };
+}
+
+module.exports = { handler };
